d106ad0ed265078df2d496bbb151aac6221010a7
Top-shagger <alan_shukur1@hotmail.com>
2024-09-19 08:40:50 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp4
+{
+	internal class Program
+	{
+		static void Main(string[] args) {
+			Time time = new();
+			//time.SetHours(5.5);
+			time.Hours = -2;
+
+
+			//Console.WriteLine("Time in hours is: " + time.GetHours());
+			Console.WriteLine($"Time in hours is: {time.Hours}" + " and time in seconds is: " + time.Seconds);
+			Console.WriteLine("Time in seconds is: " + time.Seconds);
+
+			time.Seconds = 1800;
+			Console.WriteLine("Time in hours is: " + time.Hours);
+			Console.WriteLine("Time in seconds is: " + time.Seconds);
+		}
+	}
+
+}
---
+++ ConsoleApp4/Time.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+	public class Time {
+		private double _hours;
+
+		private double _seconds;
+
+		public double Hours {
+			get {
+				return _seconds / 3600;
+			}
+
+			set {
+				if (value >= 0) {
+					_seconds = value * 3600;
+				}
+			}
+		} // dette gjør all koden under gjør
+
+		public double Seconds
+		{
+			get
+			{
+				return _seconds;
+			}
+			set
+			{
+				_seconds = value;
+			}
+		}
+
+
+		 
+/*
+		public void SetHours(double hours) {
+			if (hours >= 0) { 
+				_hours = hours;
+			}
+		}
+
+		public double GetHours() {
+			return _hours;
+		}*/
+	}
+}
